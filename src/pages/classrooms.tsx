2ff92bbc682759f82d3924d99f2c9aac005c0f2d
Alejandro Cremades <ale@alecrem.com>
2023-05-25 16:29:16 +0900

    Add main pages (no content yet)

---
+++ src/pages/classrooms.tsx
@@ -0,0 +1,24 @@
+import { Text } from '@chakra-ui/react'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { Footer } from '../components/Footer'
+import { Navigation } from '../components/Navigation'
+
+const Classrooms = () => (
+  <>
+    <Navigation />
+    <Container height="100vh">
+      <Hero title="教室一覧" />
+      <Main>
+        <Text color="text">本文です。</Text>
+      </Main>
+      <Footer>
+        <Text>© 2009-2023 Studio Kura 絵画教室 All rights reserved</Text>
+      </Footer>
+    </Container>
+  </>
+)
+
+export default Classrooms

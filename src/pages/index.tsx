6cafb783511578ce83a386032bbe5a1de5e43dff
Alejandro Cremades <ale@alecrem.com>
2023-05-25 16:20:18 +0900

    Remove unneeded import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
-import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
 import { Navigation } from '../components/Navigation'
 

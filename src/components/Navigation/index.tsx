08219b5e4c6b68633776db8b66e7172a98716086
Alejandro Cremades <ale@alecrem.com>
2023-06-15 11:03:00 +0900

    Fix Next Link components and hydration

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -61,9 +61,7 @@ function Navigation() {
             color={useColorModeValue('gray.800', 'white')}
           >
             <NextLink passHref href={'/'}>
-              <Link href={'/'}>
-                <Image src="/images/studio-kura-logo.png" height="2em" />
-              </Link>
+              <Image src="/images/studio-kura-logo.png" height="2em" />
             </NextLink>
           </Text>
 
@@ -122,11 +120,10 @@ const DesktopNav = () => {
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
           <Popover trigger={'hover'} placement={'bottom-start'}>
-            <PopoverTrigger>
-              <NextLink passHref href={navItem.href ?? '#'}>
-                <Link
+            {navItem.children ? (
+              <PopoverTrigger>
+                <Box
                   p={2}
-                  href={navItem.href ?? '#'}
                   fontSize={'sm'}
                   fontWeight={500}
                   color={linkColor}
@@ -136,9 +133,24 @@ const DesktopNav = () => {
                   }}
                 >
                   {navItem.label}
-                </Link>
+                </Box>
+              </PopoverTrigger>
+            ) : (
+              <NextLink href={navItem.href ?? '#'}>
+                <Box
+                  p={2}
+                  fontSize={'sm'}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: 'none',
+                    color: linkHoverColor
+                  }}
+                >
+                  {navItem.label}
+                </Box>
               </NextLink>
-            </PopoverTrigger>
+            )}
 
             {navItem.children && (
               <PopoverContent
@@ -166,38 +178,29 @@ const DesktopNav = () => {
 const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   return (
     <NextLink passHref href={href ?? '/'}>
-      <Link
-        href={href}
-        role={'group'}
-        display={'block'}
-        p={2}
-        rounded={'md'}
-        _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
-      >
-        <Stack direction={'row'} align={'center'}>
-          <Box>
-            <Text
-              transition={'all .3s ease'}
-              _groupHover={{ color: 'pink.400' }}
-              fontWeight={500}
-            >
-              {label}
-            </Text>
-            <Text fontSize={'sm'}>{subLabel}</Text>
-          </Box>
-          <Flex
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
             transition={'all .3s ease'}
-            transform={'translateX(-10px)'}
-            opacity={0}
-            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-            justify={'flex-end'}
-            align={'center'}
-            flex={1}
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}
           >
-            <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
-          </Flex>
-        </Stack>
-      </Link>
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}
+        >
+          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
     </NextLink>
   )
 }
@@ -231,12 +234,23 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
           textDecoration: 'none'
         }}
       >
-        <Text
-          fontWeight={600}
-          color={useColorModeValue('gray.600', 'gray.200')}
-        >
-          {label}
-        </Text>
+        {children ? (
+          <Text
+            fontWeight={600}
+            color={useColorModeValue('gray.600', 'gray.200')}
+          >
+            {label}
+          </Text>
+        ) : (
+          <NextLink href={href ?? '/'}>
+            <Text
+              fontWeight={600}
+              color={useColorModeValue('gray.600', 'gray.200')}
+            >
+              {label}
+            </Text>
+          </NextLink>
+        )}
         {children && (
           <Icon
             as={ChevronDownIcon}
@@ -259,10 +273,8 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
         >
           {children &&
             children.map((child) => (
-              <NextLink passHref href={child.href ?? '/'}>
-                <Link key={child.label} py={2} href={child.href}>
-                  {child.label}
-                </Link>
+              <NextLink key={child.label} href={child.href ?? '/'}>
+                <Box py={2}>{child.label}</Box>
               </NextLink>
             ))}
         </Stack>

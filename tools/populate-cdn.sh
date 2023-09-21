11e8d6bb63cb6d2dd78f987141032950487fbc00
Alejandro Cremades <ale@alecrem.com>
2023-09-21 15:45:46 +0900

    Bonus: script to populate CDN

---
+++ tools/populate-cdn.sh
@@ -0,0 +1,33 @@
+# ClassType cards
+wget -O "card-kodomo.jpg" https://farm5.staticflickr.com/4161/35709982975_d327d98f02_b.jpg
+wget -O "card-otona.jpg" https://www.studiokura.com/img/otona.jpg
+wget -O "card-denshi.jpg" https://www.studiokura.com/img/otona.jpg
+
+# ClassPlace cards
+wget -O "placecard-itoshima.jpg" https://www.studiokura.com/img/itoshima.jpg
+wget -O "placecard-nishijin.jpg" https://www.studiokura.com/img/itoshima.jpg
+wget -O "placecard-takeo.jpg" https://artsitoya.com/img/portfolio/artsitoya-outside2.jpg
+wget -O "placecard-karatsu.jpg" https://www.studiokura.com/img/karatsu.jpg
+wget -O "placecard-hakozaki.jpg" https://www.studiokura.com/img/hakozaki.jpg
+wget -O "placecard-hashimoto.jpg" https://www.studiokura.com/img/map-hashimoto.jpg
+wget -O "placecard-sarayama.jpg" https://www.studiokura.com/img/sarayama.jpg
+wget -O "placecard-momochi.jpg" https://www.studiokura.com/img/sarayama.jpg
+wget -O "placecard-kashii.jpg" https://www.studiokura.com/img/map-kashi.jpg
+wget -O "placecard-motooka.jpg" https://www.studiokura.com/img/map-motooka.jpg
+wget -O "placecard-befu.jpg" https://www.studiokura.com/img/map-motooka.jpg
+wget -O "placecard-kurume.jpg" https://www.studiokura.com/img/map-kurume.jpg
+wget -O "placecard-kasuya.jpg" https://www.studiokura.com/img/map-kurume.jpg
+wget -O "placecard-onojyo.jpg" https://www.studiokura.com/img/map-onojyo.jpg
+wget -O "placecard-nagazumi.jpg" https://www.studiokura.com/img/map-nagazumi.jpg
+wget -O "placecard-online.jpg" https://www.studiokura.com/img/online.jpg
+
+# ClassPlace slides: generic
+wget -O "placeslide-otona.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-otona.jpg
+wget -O "placeslide-kodomo.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-kodomo.jpg
+wget -O "placeslide-denshi.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-denshi.jpg
+
+# ClassPlace slides: Itoshima
+wget -O "placeslide1-itoshima.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-title.jpg
+wget -O "placeslide2-itoshima.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-timetable.jpg
+wget -O "placeslide2-itoshima.jpg" https://www.studiokura.com/kasuga.studiokura.com/img/itoshima-map.jpg
+wget -O "place-map-itoshima.jpg" https://www.studiokura.com/img/map-itoshima-wide.jpg

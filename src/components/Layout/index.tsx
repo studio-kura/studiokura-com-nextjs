641cc44716e141be114f71db08bc9b48ea033dde
Alejandro Cremades <ale@alecrem.com>
2023-10-19 16:23:26 +0900

    Add Layout component to add titles to all pages

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import { ReactElement, ReactNode } from 'react'
+import Head from 'next/head'
+
+interface Props {
+  title?: string
+  children: ReactElement | ReactNode
+}
+
+const Layout: React.FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <body>{children}</body>
+    </>
+  )
+}
+
+export { Layout }
+
+Layout.defaultProps = {
+  title: 'Studio Kura絵画教室 | 大人から子どもまで、絵画教室ならStudio Kura'
+}

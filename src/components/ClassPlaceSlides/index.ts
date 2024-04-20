20c0b743fc9ad893775c1bfb1d2390b8969011b6
Alejandro Cremades <ale@alecrem.com>
2024-04-20 16:21:48 +0900

    Bonus: simpler exports

--- src/components/ClassPlaceSlides/index.ts
+++ src/components/ClassPlaceSlides/index.ts
@@ -1,15 +1,6 @@
-import { ClassPlaceSlide1 } from '@/components/ClassPlaceSlides/ClassPlaceSlide1'
-import { ClassPlaceSlide2 } from '@/components/ClassPlaceSlides/ClassPlaceSlide2'
-import { ClassPlaceSlide3 } from '@/components/ClassPlaceSlides/ClassPlaceSlide3'
-import { ClassPlaceSlideOtona } from '@/components/ClassPlaceSlides/ClassPlaceSlideOtona'
-import { ClassPlaceSlideKodomo } from '@/components/ClassPlaceSlides/ClassPlaceSlideKodomo'
-import { ClassPlaceSlideDenshi } from '@/components/ClassPlaceSlides/ClassPlaceSlideDenshi'
-
-export {
-  ClassPlaceSlide1,
-  ClassPlaceSlide2,
-  ClassPlaceSlide3,
-  ClassPlaceSlideOtona,
-  ClassPlaceSlideKodomo,
-  ClassPlaceSlideDenshi
-}
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlide1'
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlide2'
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlide3'
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlideOtona'
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlideKodomo'
+export * from '@/components/ClassPlaceSlides/ClassPlaceSlideDenshi'

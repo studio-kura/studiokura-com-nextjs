7ac2907cd59cdb64281c4b0367a388bba7e0ee2f
Alejandro Cremades <ale@alecrem.com>
2024-11-08 22:13:25 +0900

    Suspend contact forms

--- src/pages/kindergartens/contact.tsx
+++ src/pages/kindergartens/contact.tsx
@@ -1,13 +1,12 @@
 import { Container } from '@/components/Container';
 import { Footer } from '@/components/Footer';
-import { KindergartenForm } from '@/components/KindergartenForm';
 import { Layout } from '@/components/Layout';
 import { Navigation } from '@/components/Navigation';
-import { Center, Heading } from '@chakra-ui/react';
+import { Center, Heading, Text } from '@chakra-ui/react';
 
 const Contact = () => {
   return (
-    <Layout title="園内レッスン お問い合わせ｜Studio Kura 絵画美術教室（福岡県　糸島市　二丈）">
+    <Layout title="お問い合わせ｜Studio Kura 絵画美術教室（福岡県　糸島市　二丈）">
       <Navigation />
       <Container height={'200px'} minW={'100%'}>
         <Heading mt={'1em'} size={['2xl', '2xl', '2xl']}>
@@ -16,7 +15,12 @@ const Contact = () => {
       </Container>
       <Center minW={'100%'}>
         <Container minW={'100%'} paddingX={'0px'} marginX={'0px'}>
-          <KindergartenForm variant="contact" />
+          <Text>
+            下記のメールアドレスにてお問い合わせ内容をご記入のうえ、送信してください。
+          </Text>
+          <Text mt="1em">
+            <a href="mailto:info@studiokura.com">info@studiokura.com</a>
+          </Text>
         </Container>
       </Center>
       <Container>
@@ -27,3 +31,34 @@ const Contact = () => {
 };
 
 export default Contact;
+
+// TODO: Restore when the following issue is solved:
+// import { Container } from '@/components/Container';
+// import { Footer } from '@/components/Footer';
+// import { KindergartenForm } from '@/components/KindergartenForm';
+// import { Layout } from '@/components/Layout';
+// import { Navigation } from '@/components/Navigation';
+// import { Center, Heading } from '@chakra-ui/react';
+
+// const Contact = () => {
+//   return (
+//     <Layout title="園内レッスン お問い合わせ｜Studio Kura 絵画美術教室（福岡県　糸島市　二丈）">
+//       <Navigation />
+//       <Container height={'200px'} minW={'100%'}>
+//         <Heading mt={'1em'} size={['2xl', '2xl', '2xl']}>
+//           園内レッスン お問い合わせ
+//         </Heading>
+//       </Container>
+//       <Center minW={'100%'}>
+//         <Container minW={'100%'} paddingX={'0px'} marginX={'0px'}>
+//           <KindergartenForm variant="contact" />
+//         </Container>
+//       </Center>
+//       <Container>
+//         <Footer />
+//       </Container>
+//     </Layout>
+//   );
+// };
+
+// export default Contact;

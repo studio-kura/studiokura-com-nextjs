9d199f0bebc2e6c7914a9a2a3bbca37477cdc0a6
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2025-07-26 11:05:07 +0900

    TomariPlace追加

---
+++ src/pages/tomari.tsx
@@ -0,0 +1,45 @@
+import { Container } from '@chakra-ui/react';
+
+import {
+  ClassPlaceSlide1,
+  ClassPlaceSlide2,
+  ClassPlaceSlide3,
+  // ClassPlaceSlideOtona,
+  ClassPlaceSlideKodomo,
+} from '@/components/ClassPlaceSlides/';
+import { Footer } from '@/components/Footer';
+import { Layout } from '@/components/Layout';
+import { Navigation } from '@/components/Navigation';
+
+const Hakozaki2Place = () => (
+  <Layout title="Studio Kura 絵画教室 泊校">
+    <Navigation />
+    <ClassPlaceSlide1
+      placeName="Studio Kura 泊校"
+      tagline="子ども絵画造形教室"
+      bgImageUrl="placeslide1-tomari.jpg"
+    >
+      {/* メモ（トップスライドに反映） */}
+    </ClassPlaceSlide1>
+    <ClassPlaceSlide2
+      placeName="Studio Kura 泊校"
+      tagline="子ども絵画造形教室"
+      bgImageUrl="placeslide2-hakozaki2.jpg"
+      calendarUrl="https://online.studiokura.com/cake/class_places/calendarwidget/hakozaki2"
+    ></ClassPlaceSlide2>
+    <ClassPlaceSlide3
+      placeName="Studio Kura 泊校"
+      tagline="子ども絵画造形教室"
+      bgImageUrl="placeslide3-hakozaki2.jpg"
+      mapImageUrl="place-map-hakozaki2-big.jpg"
+      mapUrl="https://www.google.com/maps/place/%E6%B3%8A%E4%B8%80%E5%85%AC%E6%B0%91%E9%A4%A8/@33.5473741,130.1354197,13z/data=!4m10!1m2!2m1!1z5rOK5YWs5rCR6aSo!3m6!1s0x3541e9d80d66c8db:0x654e3c719518bac8!8m2!3d33.5769699!4d130.2088542!15sCgzms4rlhazmsJHppKiSARBjb21tdW5pdHlfY2VudGVyqgFJEAEqESIN5rOKIOWFrOawkemkqCgIMh8QASIbN_5HvZwp8kCHuD72dzF9dXafQczZer1DboNjMhEQAiIN5rOKIOWFrOawkemkqOABAA!16s%2Fg%2F11trfk1ypj?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D"
+      address="福岡県福岡市東区箱崎２丁目１０−３５"
+    ></ClassPlaceSlide3>
+    <ClassPlaceSlideKodomo />
+    <Container>
+      <Footer />
+    </Container>
+  </Layout>
+);
+
+export default TomariPlace;

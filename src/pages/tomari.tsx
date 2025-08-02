d19b3a3785e64b57e045b9a731cec2e2dcada0d0
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2025-08-02 20:06:02 +0900

    エラー修正

--- src/pages/tomari.tsx
+++ src/pages/tomari.tsx
@@ -11,7 +11,7 @@ import { Footer } from '@/components/Footer';
 import { Layout } from '@/components/Layout';
 import { Navigation } from '@/components/Navigation';
 
-const Hakozaki2Place = () => (
+const TomariPlace = () => (
   <Layout title="Studio Kura 絵画教室 泊校">
     <Navigation />
     <ClassPlaceSlide1
@@ -24,16 +24,16 @@ const Hakozaki2Place = () => (
     <ClassPlaceSlide2
       placeName="Studio Kura 泊校"
       tagline="子ども絵画造形教室"
-      bgImageUrl="placeslide2-hakozaki2.jpg"
-      calendarUrl="https://online.studiokura.com/cake/class_places/calendarwidget/hakozaki2"
+      bgImageUrl="placeslide2-tomari.jpg"
+      calendarUrl="https://online.studiokura.com/cake/class_places/calendarwidget/tomari"
     ></ClassPlaceSlide2>
     <ClassPlaceSlide3
       placeName="Studio Kura 泊校"
       tagline="子ども絵画造形教室"
-      bgImageUrl="placeslide3-hakozaki2.jpg"
-      mapImageUrl="place-map-hakozaki2-big.jpg"
+      bgImageUrl="placeslide3-tomari.jpg"
+      mapImageUrl="place-map-tomari-big.jpg"
       mapUrl="https://www.google.com/maps/place/%E6%B3%8A%E4%B8%80%E5%85%AC%E6%B0%91%E9%A4%A8/@33.5473741,130.1354197,13z/data=!4m10!1m2!2m1!1z5rOK5YWs5rCR6aSo!3m6!1s0x3541e9d80d66c8db:0x654e3c719518bac8!8m2!3d33.5769699!4d130.2088542!15sCgzms4rlhazmsJHppKiSARBjb21tdW5pdHlfY2VudGVyqgFJEAEqESIN5rOKIOWFrOawkemkqCgIMh8QASIbN_5HvZwp8kCHuD72dzF9dXafQczZer1DboNjMhEQAiIN5rOKIOWFrOawkemkqOABAA!16s%2Fg%2F11trfk1ypj?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D"
-      address="   〒819-1111 福岡県糸島市泊１２１６−２"
+      address="〒819-1111 福岡県糸島市泊１２１６−２"
     ></ClassPlaceSlide3>
     <ClassPlaceSlideKodomo />
     <Container>
